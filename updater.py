b92e2cdff89e947ee00221c8f1421534c156069d
PSavvateev <p.savvateev@gmail.com>
2022-07-18 23:50:06 +0200

    improved updater module and status updating function

--- updater.py
+++ updater.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 import requests
 import psycopg2
+from bs4 import BeautifulSoup
 
 
 load_dotenv()
@@ -14,63 +15,59 @@ db_name = os.environ["POSTGRESQL_NAME"]
 URI = f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
 
 
-SELECT_JOBURL = """SELECT job_id, job_url FROM jobs WHERE
-               (age(current_date, job_date) >= '90 days') 
-               AND (job_status = 'active')
-               """
+class DBUpdater:
+    def __init__(self):
+        self.connection = psycopg2.connect(URI)
 
-SELECT_CITIES = "SELECT id, city, admin_name FROM cities"
+    SELECT_JOB_TO_UPDATE = """SELECT job_id, job_url FROM jobs WHERE
+                   (age(current_date, job_date) >= '60 days') 
+                   AND (job_status = 'active')
+                   """
+    UPDATE_STATUS = """UPDATE jobs
+                        SET job_status = %s
+                        WHERE job_id = %s
+                    """
 
-SELECT_EMPTY_CITIES = "SELECT job_id, job_loc, city_id FROM jobs WHERE city_id is NULL"
+    def update_status(self):
+        """
+        Check and updates all the existent jobs in the database as active/inactive.
 
-UPDATE_STATUS = """UPDATE jobs
-                    SET job_status = %s
-                    WHERE job_id = %s
-                """
-UPDATE_CITY = """UPDATE jobs
-                SET city_id = ?
-                WHERE job_id = ?
-                """
+        :return: None
+        """
 
-UPDATE_DATES = """UPDATE jobs 
-                SET job_date =?
-                WHERE job_id = ?
-                """
+        print('Start status updating')
 
-connection = psycopg2.connect(URI)
+        with self.connection as conn:
+            with conn.cursor() as cursor:
+                cursor.execute(self.SELECT_JOB_TO_UPDATE)
+                urls = cursor.fetchall()
+                print(f'Records found: {len(urls)}')
 
-def update_status():
-    print('Start Updating')
+                for url in urls:
+                    print(f'job_id: {url[0]}')
+                    try:
+                        response = requests.get(url[1])
+                        soup = BeautifulSoup(response.text, 'html.parser')
+                        alert = soup.find('div', 'jobsearch-JobInfoHeader-expiredHeader').h3.text
 
-    with connection as conn:
-        with conn.cursor() as cursor:
-            cursor.execute(SELECT_JOBURL)
-            urls = cursor.fetchall()
-            print(f'Records found: {len(urls)}')
+                    except AttributeError:
+                        print('Still active: pass')
+                        continue
 
-            for url in urls:
-                print(f'job_id: {url[0]}')
-                try:
-                    requests.get(url[1])
-                except requests.exceptions.ConnectionError:
-                    cursor.execute(UPDATE_STATUS, ('inactive', url[0]))
-                    print('updated')
+                    else:
+                        cursor.execute(self.UPDATE_STATUS, ('inactive', url[0]))
+                        print('Deactivated: updated')
 
-                else:
-                    print('pass')
-                    continue
+                    finally:
+                        continue
+        print('End of updating session.')
 
-                finally:
-                    continue
-    print('End')
+    def update_city(self):
+        pass
 
-
-update_status()
-
-
-def update_city():
-    pass
+    def update_dates(self):
+        pass
 
 
-def update_dates():
-    pass
+new_update = DBUpdater()
+new_update.update_status()
